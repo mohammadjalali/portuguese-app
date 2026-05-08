fe139a4ce200e2e98f5804af543da9fbb66eee46
mohammadhosein jalali <mjalali1378@gmail.com>
2026-05-08 20:44:45 +0100

    added sessions 5-10

--- src/data/index.js
+++ src/data/index.js
@@ -3,6 +3,12 @@ import session1 from "./sessions/session1.json";
 import session2 from "./sessions/session2.json";
 import session3 from "./sessions/session3.json";
 import session4 from "./sessions/session4.json";
+import session5 from "./sessions/session5.json";
+import session6 from "./sessions/session6.json";
+import session7 from "./sessions/session7.json";
+import session8 from "./sessions/session8.json";
+import session9 from "./sessions/session9.json";
+import session10 from "./sessions/session10.json";
 
-export const sessions = [session1, session2, session3, session4];
+export const sessions = [session1, session2, session3, session4, session5, session6, session7, session8, session9, session10];
 export { courseInfo };
